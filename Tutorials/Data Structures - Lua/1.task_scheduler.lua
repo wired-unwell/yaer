70cf00845d99d79314f0a823904b2d4d98aa8c86
Behnam <151406767+wired-unwell@users.noreply.github.com>
2024-11-08 20:37:52 +0330

    Cleaned up and added lua-lsp comments.

--- Tutorials/Data Structures - Lua/1.task_scheduler.lua
+++ Tutorials/Data Structures - Lua/1.task_scheduler.lua
@@ -9,19 +9,12 @@
    A simple task scheduler that allows use of dependencies and priprities.
 --]]
 
-
---[[
-local tasks = {}
-local task = {
-   id = 0,
-   pri = 3,
-   dep = {1,2,3},
-   desc = 'Shit',
-}
-table.insert(tasks,t1)
-tasks[task.id]=task
---]]
-
+---Print Tables
+---usually used for debugging.
+---@generic Table Print
+---@param tab table The table
+---@param ind? string Indentation
+---@param line? string Separator
 function tprint(tab,ind,line)
    local ind = ind or ''
    local line = line or '==='
@@ -32,56 +25,21 @@ function tprint(tab,ind,line)
    end
 end
 
---[[
-table.insert(tasks,{id=1,pri=3,job=8,dep=1})
-table.insert(tasks,{id=3,pri=1,job=8,dep=2})
-table.insert(tasks,{pri=0,job=8,dep=1})
--- tprint(tasks)
---]]
-
--- wtf am I even doing?
---[[
-   high pro tasks first.
-   deps done? -> do it
-   deps notyet? -> do deps
-   come back to task
-   next pri
---]]
-
---[[
-local task_low={}
-local task_med={}
-local task_high={}
-local task_db={task_low,task_med,task_high}
-
-task_db.new = function (task)
-   task.pri = task.pri or 1
-   table.insert(task_db[task.pri],task)
-end
-
-task_db.new({id='fack',pri=2,desc="I'm a task"})
-
-task_db.new({id='noway',pri=3,desc="I'm a NEW AND BETTER task",dep={'fack'}})
-
-tprint(task_db)
-
--- while true do
-for i=3,1,-1 do
-   print(i)
-   -- OOOH. IF I SEPARATE TASKS IN DIFFERENT INDEXES, I CAN'T LINK BACK DEPS
-end
--- end
-
---]]
-
-
 --- Initial working version
+
+---@class task
+---@field id? integer
+---@field desc string
+---@field done? boolean
+---@field deps? table
 
 local tasks = {}
 
 -- I had indeed added some crappy implementation, where you could assign bad task ids.
 -- Now the simple reassignment makes the id value unnecessary
 
+---Add task to my tasks
+---@param task task
 function tasks.add (task)
    task.id=#tasks+1
    tasks[task.id]=task
@@ -106,26 +64,19 @@ function tasks.process(id)
    task.done=true
 end
 
-function tasks.resolve_deps(id)
-   local task = tasks.id
-   for _,n in pairs(task.deps) do
-      tasks.process(n.id)
-   end
-end
-
 function tasks.processAll()
    for _,task in ipairs(tasks) do
       -- print(task.desc .. " try")
       if task.done ~= true
 	 and (task.deps.resolved==true or task.deps==nil or #task.deps==0) then
 	 tasks.process(task.id)
-	 
+
       elseif task.done ~= true and task.deps.resolved ~= true then
 	 for _,dep_id in pairs(task.deps) do
 	    if tasks[dep_id].done ~=true then
 	       tasks.process(dep_id)
 	    end
-	 end	 
+	 end
 	 tasks.process(task.id)
 
       elseif task.done ~= true then
